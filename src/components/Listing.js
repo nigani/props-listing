f76d6c88dc9c363550c7e952ac89548f5e16eea0
nigani <nigani@internet.ru>
2022-06-16 23:48:35 +0300

    1 file modified

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Item from './Item.js';
-import { nanoid } from 'nanoid';
 
 export default function Listing(props) {
   const { items } = props;
     return (
       <div className="item-list">
         {items.map((item) => (
-          <Item item={item} key={nanoid()} />
+          <Item item={item} key={item.listing_id} />
         ))}
       </div>
     );
